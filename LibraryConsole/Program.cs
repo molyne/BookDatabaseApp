d1015dc72841e7151847a932b3df4bb76e62575e
Molyn Eriksson <molyneriksson@gmail.com>
2018-11-14 20:27:57 +0100

    console app added

---
+++ LibraryConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using LibraryConsole.ServiceReference1;
+using System;
+using System.Linq;
+
+namespace LibraryConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceReference1.BookDatabaseServiceClient proxy = new BookDatabaseServiceClient())
+            {
+                var books = proxy.GetBooks();
+                Console.WriteLine(books.Select(t => t.Title).First());
+                Console.ReadKey();
+            }
+        }
+    }
+}
